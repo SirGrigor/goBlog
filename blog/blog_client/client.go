a14e515e2821deeed9f931c8526031cad1d60007
Ilja <ilja@shopz.com>
2021-04-26 16:12:01 +0300

    Reader controller

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,1 +1,49 @@
-package blog_client
+package main
+
+import (
+	"context"
+	"fmt"
+	"goBlog/blog/blogpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func createBlog(err error, c blogpb.BlogServiceClient, blog *blogpb.Blog) *blogpb.CreateBlogResponse {
+	createBlog, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpercted Error: %v", err)
+	}
+	fmt.Printf("Blog has been created: %v", createBlog)
+
+	return createBlog
+}
+
+func main() {
+	log.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("Cannot connect: %v", err)
+	}
+	defer cc.Close()
+	c := blogpb.NewBlogServiceClient(cc)
+	blog := &blogpb.Blog{
+		AuthorId: "Stephane",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog"}
+
+	log.Println("Creating the Blog")
+	//Create Blog
+	createblog := createBlog(err, c, blog)
+	// ReadBlog
+
+	readBlog, err := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "21414513"})
+	if err != nil {
+		fmt.Printf("Error happened while reading: %v", err)
+	}
+
+	readBlogReg = &blogpb.ReadBlogRequest{BlogId: }
+	c.ReadBlog(context.Background(), )
+}
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,25 +1,122 @@
-package blog_server
+package main
 
 import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"goBlog/blog/blogpb"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 )
 
+var collection *mongo.Collection
+
 type server struct {
 }
 
-func main()  {
-	log.Println("Blog Server Started")
+func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
+	blogId := req.BlogId
+	oid, err := primitive.ObjectIDFromHex(blogId)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Cannot Parse Id: %v", err))
+	}
+	data := &blogItem{}
+	filter := bson.D{{"_id", oid}}
+	res := collection.FindOne(context.Background(), filter)
+	if err := res.Decode(data); err != nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Cannot Find doc with specified ID: %v", err))
+	}
+	return &blogpb.ReadBlogResponse{Blog: &blogpb.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorID,
+		Title:    data.Title,
+		Content:  data.Content,
+	},
+	}, nil
+}
 
+func (*server) CreateBlog(ctx context.Context, request *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	blog := request.GetBlog()
+
+	log.Println("Create a blog request")
+	data := blogItem{
+		AuthorID: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,
+			fmt.Sprintf("Internal error: %v", err))
+	}
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(codes.Internal,
+			fmt.Sprintf("Cannot coverto to OID: %v", err))
+	}
+
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: blog.AuthorId,
+			Title:    blog.Title,
+			Content:  blog.Content,
+		},
+	}, nil
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"id, omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func connectMongo() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	collection = client.Database("mydb").Collection("blog")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB")
+	return client
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	mongoClient := connectMongo()
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to start server with error: %v", err)
 	}
 
+	log.Println("Blog Server Started")
 	var opts []grpc.ServerOption
 	s := grpc.NewServer(opts...)
 	blogpb.RegisterBlogServiceServer(s, &server{})
@@ -40,5 +137,7 @@ func main()  {
 	s.Stop()
 	log.Println("Stopping the listener")
 	lis.Close()
+	log.Println("Closing MongoDB")
+	mongoClient.Disconnect(context.TODO())
 	log.Println("End of Program")
 }
